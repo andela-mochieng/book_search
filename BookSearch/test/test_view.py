08ad3f3729f1cb0c15c1a4f40f02a7f7721db0c4
andela-mochieng <margaret.ochieng@andela.com>
2016-11-21 19:45:04 +0300

    [Fix] corrected the tests and aded the requirements file

--- BookSearch/test/test_view.py
+++ BookSearch/test/test_view.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from django.core.urlresolvers import reverse 
+from django.core.urlresolvers import reverse
 from search.models import Category, Book
 
 
@@ -8,24 +8,40 @@ class TestSearchView(TestCase):
     def setUp(self):
         self.client = Client()
         self.search_url = reverse('search')
-        self.book_title = {'title': 'Test book title'}
+        self.book_title = 'TestBook'
+        self.book_title2 = {'title': 'TestBook'}
         self.category_name = {'name': 'History'}
-        self.create_category = Category.objects.create(name=self.category_name)
-        self.create_book = Book.objects.create(name=self.book_title, category=self.create_category)
+        self.category_name2 = 'History'
+        self.create_category = Category.objects.create(
+            name=self.category_name2)
+        self.create_book = Book.objects.create(
+            title=self.book_title, category=self.create_category)
 
-    def test_book_search_successfull(self):
+    def test_booktitle_partial_search_successfully(self):
         """Test available book is returned when searched"""
-        response = self.client.get(self.search_url, self.book_title)
+        response = self.client.get(self.search_url, self.book_title2)
         self.assertTrue(response.status_code, 200)
-        self.assertIn(response.values()['request'], self.book_title)
-        
+        self.assertIn('Test', str(response.context['request']))
 
+    def test_book_search_successfully(self):
+        """Test available book is returned when searched"""
+        response = self.client.get(self.search_url, self.book_title2)
+        self.assertTrue(response.status_code, 200)
+        self.assertIn(self.book_title, str(response.context['request']))
 
+    def test_partial_category_name_search_is_unsucessful(self):
+        """Test partial category name search is unsucessful"""
+        response = self.client.get(self.search_url, self.category_name)
+        self.assertNotIn('histo', str(response.context['request']))
 
-    def test_book_search_unsuccessful(self):
-        """Test unavailable book returns 404 """
+    def test_exact_category_name_search_is_successfully(self):
+        """Test exact category name search is successfully"""
+        response = self.client.get(self.search_url, self.category_name)
+        self.assertTrue(response.status_code, 200)
+        self.assertIn(self.category_name2, str(response.context['request']))    
+
+    def test_book_search_unsuccessfully(self):
+        """Test search unavailable book returns all the books """
         response = self.client.get(self.search_url, {'title': 'Anything test'})
-        self.assertTrue(response.status_code, 404 )
-
-
-            
+        all_books = Book.objects.all()
+        self.assertTrue(response.status_code, all_books)
